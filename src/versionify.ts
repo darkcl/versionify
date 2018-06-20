a34e8fad92d9b344d26c7d83e356158f9d065eb5
Yeung Yiu Hung <yyeung@ebay.com>
2018-06-20 11:55:51 +0800

    Add tests

--- src/versionify.ts
+++ src/versionify.ts
@@ -1,11 +1,22 @@
+import { join } from "path";
+
+import * as fs from "fs";
+
 export class Versionify {
-  private greeting: string;
+  constructor(private version: string) {}
 
-  constructor(message: string) {
-    this.greeting = message;
-  }
+  public invoke(path: string, root: string, funcName: string) {
+    const allVersions: string[] = fs
+      .readdirSync(path)
+      .filter(val => val.indexOf(root) !== -1)
+      .sort();
 
-  public greet(): string {
-    return `Bonjour, ${this.greeting}!`;
+    try {
+      const result = require(join(path, `${root}.${this.version}`));
+      return result[funcName];
+    } catch (e) {
+      const result = require(join(path, allVersions[allVersions.length - 1]));
+      return result[funcName];
+    }
   }
 }
--- test/versionify-spec.ts
+++ test/versionify-spec.ts
@@ -1,11 +1,35 @@
 import { Versionify } from "../src";
-import * as chai from "chai";
-
-const expect = chai.expect;
+import { expect } from "chai";
+import { join } from "path";
 
 describe("Versionify", () => {
-  it("should greet with message", () => {
-    const greeter = new Versionify("friend");
-    expect(greeter.greet()).to.equal("Bonjour, friend!");
+  it("should read respective version function", () => {
+    const sut: Versionify = new Versionify("1.0.0");
+
+    const funcV1: Function = sut.invoke(
+      join(__dirname, "./fixture"),
+      "func",
+      "hello"
+    );
+    expect(funcV1()).to.equal("hello 1.0.0");
+
+    const sut2: Versionify = new Versionify("1.0.1");
+    const funcV101: Function = sut2.invoke(
+      join(__dirname, "./fixture"),
+      "func",
+      "hello"
+    );
+    expect(funcV101()).to.equal("hello 1.0.1");
+  });
+
+  it("should fallback version function", () => {
+    const sut: Versionify = new Versionify("1.0.2");
+
+    const funcV1: Function = sut.invoke(
+      join(__dirname, "./fixture"),
+      "func",
+      "hello"
+    );
+    expect(funcV1()).to.equal("hello 1.0.1");
   });
 });
